c901e7e180ab60090500d2fc89332506a3124636
Yusuke Hosonuma <tobi462@gmail.com>
2021-05-12 20:44:05 +0900

    fix

--- samples/client/petstore/crystal/src/petstore/api_client.cr
+++ samples/client/petstore/crystal/src/petstore/api_client.cr
@@ -171,7 +171,7 @@ module Petstore
     # @param [Hash] query_params Query parameters
     # @param [String] auth_names Authentication scheme name
     def update_params_for_auth!(header_params, query_params, auth_names)
-      Array{auth_names}.each do |auth_name|
+      auth_names.each do |auth_name|
         auth_setting = @config.auth_settings[auth_name]
         next unless auth_setting
         case auth_setting[:in]
